ba0de65786bf032d8b06bd3a1dfe1db976e0e8cb
gorkemyagci <gorkemyagci705@gmail.com>
2024-10-09 23:12:26 +0300

    react-fiber

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,177 @@
-import Image from "next/image";
+"use client";
+import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { useRef, useState } from "react";
+import {
+  MeshWobbleMaterial,
+  OrbitControls,
+  useHelper,
+} from "@react-three/drei";
+import { Leva, useControls } from "leva";
+
+const Cube = ({
+  position = [0, 0, 0],
+  color = "orange",
+  size,
+}: {
+  position?: [number, number, number];
+  color?: string;
+  size: [number, number, number];
+}) => {
+  const ref = useRef<THREE.Mesh>(null);
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.y += delta;
+      ref.current.rotation.x += delta * 2.0;
+      ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
+    }
+  });
+  return (
+    <mesh position={position} ref={ref}>
+      <boxGeometry args={size} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+
+const Sphere = ({
+  position,
+  size,
+  color,
+}: {
+  position: [number, number, number];
+  size: [number, number, number];
+  color: string;
+}) => {
+  const ref = useRef<THREE.Mesh>(null);
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+  useFrame((state, delta) => {
+    const speed = isHovered ? 1 : 0.2;
+    if (ref.current) {
+      ref.current.rotation.y += delta * speed;
+    }
+  });
+  return (
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
+      onPointerLeave={(event) => (event.stopPropagation(), setIsHovered(false))}
+      onClick={(event) => (event.stopPropagation(), setIsClicked(!isClicked))}
+      scale={isClicked ? 1.5 : 1}
+    >
+      <sphereGeometry args={size} />
+      <meshStandardMaterial
+        color={isHovered ? "orange" : "lightblue"}
+        wireframe
+      />
+    </mesh>
+  );
+};
+
+const Torus = ({
+  position,
+  size,
+  color,
+}: {
+  position: [number, number, number];
+  size: [number, number, number, number];
+  color: string;
+}) => {
+  const ref = useRef<THREE.Mesh>(null);
+  useFrame((state, delta) => {
+    if (ref.current) {
+      ref.current.rotation.y += delta;
+      ref.current.rotation.x += delta * 2.0;
+      ref.current.position.z = Math.sin(state.clock.elapsedTime) * 4;
+    }
+  });
+  return (
+    <mesh position={position} ref={ref}>
+      <torusGeometry args={size} />
+      <meshStandardMaterial color={color} wireframe />
+    </mesh>
+  );
+};
+
+const TorusKnot = ({
+  position,
+  size,
+  color,
+}: {
+  position: [number, number, number];
+  size: [number, number, number, number];
+  color: string;
+}) => {
+  const ref = useRef<THREE.Mesh>(null);
+  // useFrame((state, delta) => {
+  //   if (ref.current) {
+  //     ref.current.rotation.y += delta;
+  //     ref.current.rotation.x += delta * 2.0;
+  //     ref.current.position.z = Math.sin(state.clock.elapsedTime) * 4;
+  //   }
+  // });
+  return (
+    <mesh position={position} ref={ref}>
+      <torusKnotGeometry args={size} />
+      {/* <meshStandardMaterial color={color} wireframe /> */}
+      <MeshWobbleMaterial factor={3} speed={2} wireframe />
+    </mesh>
+  );
+};
+
+const Scene = () => {
+  const directionalLightRef = useRef<THREE.DirectionalLight>(null);
+  useHelper(
+    directionalLightRef as React.MutableRefObject<THREE.Object3D>,
+    THREE.DirectionalLightHelper,
+    0.5,
+    "white"
+  );
+  const { lightColor, lightIntensity } = useControls({
+    lightColor: "white",
+    lightIntensity: {
+      value: 0.5,
+      min: 0.5,
+      max: 5,
+    },
+  });
+  return (
+    <>
+      <directionalLight
+        intensity={lightIntensity}
+        position={[0, 0, 5]}
+        ref={directionalLightRef}
+        color={lightColor}
+      />
+      <ambientLight intensity={0.1} />
+      <group position={[0, -1, 0]}>
+        <Cube position={[1, 0, 0]} color="green" size={[1, 1, 1]} />
+        <Cube position={[-1, 0, 0]} color="blue" size={[1, 1, 1]} />
+        <Cube position={[-1, 2, 0]} color="red" size={[1, 1, 1]} />
+        <Cube position={[1, 2, 0]} color="hotpink" size={[1, 1, 1]} />
+      </group>
+      <Cube position={[0, 0, 0]} color="orange" size={[1, 1, 1]} />
+      <Sphere position={[0, 0, 0]} size={[1, 30, 30]} color="orange" />
+      <Torus position={[2, 0, 0]} size={[0.8, 0.1, 30, 30]} color="green" />
+      <TorusKnot
+        position={[0, 0, 0]}
+        size={[1, 0.1, 1000, 50]}
+        color="hotpink"
+      />
+      <OrbitControls />
+    </>
+  );
+};
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <Leva collapsed />
+      <Canvas camera={{ position: [0, 0, 7], fov: 100 }}>
+        <Scene />
+      </Canvas>
     </div>
   );
 }
